d40164502d68f1cc4e511e58b6de2cb87b3df722
YUI-TIEN <youwei0112@gmail.com>
2025-05-26 11:38:24 +0800

    新增部署到pages 的 action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',   // generate static files suitable for GitHub Pages
-  base: '/',          // root path; adjust if deploying to a subdirectory
+  site: "https://yui-tien.github.io/morphusai-kiki/", // 或是你的自訂域名
+  base: "/morphusai-kiki", // 如果需要的話，設定為你的專案名稱，開頭加上斜線
 });
